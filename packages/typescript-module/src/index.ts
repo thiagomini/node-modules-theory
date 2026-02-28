49149d6cfe59fcf79e4cb586cacb8925e32909f4
Thiago Martins <thiago_valentim1@hotmail.com>
2026-02-28 09:37:19 -0300

    feat: add Options interface and defaultOptions constant; update index and lockfile

--- packages/typescript-module/src/index.ts
+++ packages/typescript-module/src/index.ts
@@ -1,5 +1,7 @@
 import { format } from './format.ts';
 import { run } from './job.ts';
+import { type Options, defaultOptions } from './interface.ts';
 
 console.log(format('Hello, TypeScript module system!'));
+console.log('Default options:', defaultOptions);
 run();
---
+++ packages/typescript-module/src/interface.ts
@@ -0,0 +1,7 @@
+export interface Options {
+  value: string;
+}
+
+export const defaultOptions: Options = {
+  value: 'Default value',
+};
